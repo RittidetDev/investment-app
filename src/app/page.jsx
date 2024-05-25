9ef054e31cc2c1ccf974d0d4f34410cd23b295b9
rittidetnpk1 <rittidetnpk1@gmail.com>
2024-05-26 02:16:59 +0700

    [Update] table

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState } from 'react';
-import { Button, Col, Container, Dropdown, DropdownButton, Row, Tab, Tabs } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Col, Container, Dropdown, DropdownButton, Pagination, Row, Tab, Table, Tabs } from 'react-bootstrap';
 
 export default function Home() {
 
@@ -68,6 +68,39 @@ export default function Home() {
   ]
   const [changeApproveSelected, setChangeApproveSelected] = useState(changeApproveList[0].key)
 
+  const [memberList, setMemberList] = useState([
+    {}, {}, {}, {}, {},
+    {}, {}, {}, {}, {},
+    {}, {}, {}, {}, {},
+    {}, {}, {}, {}, {},
+    {}, {}, {}, {}, {},
+    {}, {}, {}, {}, {},
+  ])
+  const [dataPerPage, setDatePerPage] = useState([[]])
+  const [currentIndexPage, setCurrentIndexPage] = useState(0)
+
+  useEffect(() => {
+    const dataPerPage = memberList.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / Number(perPageSelected))
+
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = [] // start a new chunk
+      }
+
+      resultArray[chunkIndex].push(item)
+
+      return resultArray
+    }, [])
+    setCurrentIndexPage(0)
+    setDatePerPage(dataPerPage)
+  }, [perPageSelected, memberList])
+
+  const renderDivider = () => {
+    return (
+      <div style={{ borderBottom: '1px solid #D7D8DA', marginTop: 12, marginBottom: 12 }} />
+    )
+  }
+
   const renderDropdown = (list, state, setState) => {
 
     const title = list.find(el => el.key == state)
@@ -92,6 +125,119 @@ export default function Home() {
     )
   }
 
+  const renderHeader = () => {
+    return (
+      <>
+        <Row>
+          <Col xs={7}>
+            신청 목록 (총 100명 | 승인대기 1건)
+          </Col>
+          <Col xs={5}>
+            <Row>
+              <Col xs={4}>
+                {renderDropdown(approveList, approveSelected, setApproveSelected)}
+              </Col>
+              <Col xs={4}>
+                {renderDropdown(dateList, dateSelected, setDateSelected)}
+              </Col>
+              <Col xs={4}>
+                {renderDropdown(perPageList, perPageSelected, setPerPageSelected)}
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        {renderDivider()}
+        <Row>
+          <Col xs={5}>
+            <Row>
+              <Col xs={3}>
+                <Button variant="primary" active style={{ width: '100%' }}>
+                  등록
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={2} />
+          <Col xs={5}>
+            <Row>
+              <Col style={{ textAlign: 'right', alignSelf: 'center' }}>
+                선택한 0건
+              </Col>
+              <Col xs={4}>
+                {renderDropdown(changeApproveList, changeApproveSelected, setChangeApproveSelected)}
+              </Col>
+              <Col xs={3}>
+                <Button variant="primary" active style={{ width: '100%' }}>
+                  등록
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </>
+    )
+  }
+
+  const renderTable = () => {
+    return (
+      <Table striped className='mt-3'>
+        <thead>
+          <tr>
+            <th><input type="checkbox" /></th>
+            <th>NO</th>
+            <th>기존유형</th>
+            <th>신청유형</th>
+            <th>제출서류</th>
+            <th>신청일시</th>
+            <th>승인여부</th>
+            <th>승인거부 사유</th>
+            <th>승인일시</th>
+            <th>관리자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            dataPerPage[currentIndexPage].map((member, index) => (
+              <tr key={`member-${index}`}>
+                <td><input type="checkbox" /></td>
+                <td>{index + 1}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+    )
+  }
+
+  const renderPagination = () => {
+    return (
+      <Pagination className="justify-content-md-center">
+        <Pagination.First />
+        <Pagination.Prev />
+        {
+          dataPerPage.map((page, index) => (
+            <Pagination.Item
+              active={index == currentIndexPage}
+              onClick={() => setCurrentIndexPage(index)}
+            >
+              {index + 1}
+            </Pagination.Item>
+          ))
+        }
+        <Pagination.Next />
+        <Pagination.Last />
+      </Pagination>
+    )
+  }
+
   return (
     <Container style={{ marginTop: 20, marginBottom: 20 }}>
       <Tabs
@@ -105,52 +251,9 @@ export default function Home() {
           기본정보 관리
         </Tab>
         <Tab eventKey="member" title="투자유형 관리">
-          <Row>
-            <Col xs={7}>
-              신청 목록 (총 100명 | 승인대기 1건)
-            </Col>
-            <Col xs={5}>
-              <Row>
-                <Col xs={4}>
-                  {renderDropdown(approveList, approveSelected, setApproveSelected)}
-                </Col>
-                <Col xs={4}>
-                  {renderDropdown(dateList, dateSelected, setDateSelected)}
-                </Col>
-                <Col xs={4}>
-                  {renderDropdown(perPageList, perPageSelected, setPerPageSelected)}
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <div style={{ borderBottom: '1px solid #D7D8DA', marginTop: 12, marginBottom: 12 }} />
-          <Row>
-            <Col xs={5}>
-              <Row>
-                <Col xs={3}>
-                  <Button variant="primary" active style={{ width: '100%' }}>
-                    등록
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-            <Col xs={2} />
-            <Col xs={5}>
-              <Row>
-                <Col style={{ textAlign: 'right', alignSelf: 'center' }}>
-                  선택한 0건
-                </Col>
-                <Col xs={4}>
-                  {renderDropdown(changeApproveList, changeApproveSelected, setChangeApproveSelected)}
-                </Col>
-                <Col xs={3}>
-                  <Button variant="primary" active style={{ width: '100%' }}>
-                    등록
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+          {renderHeader()}
+          {renderTable()}
+          {memberList.length > 0 ? renderPagination() : null}
         </Tab>
         <Tab eventKey="transfer" title="입출금내역 조회">
           입출금내역 조회
